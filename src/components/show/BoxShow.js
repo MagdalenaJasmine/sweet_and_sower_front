30796e9d0f7265ea0a6d73a64408f4943aa06197
MagdalenaJasmine <magdalenacooper92@gmail.com>
2020-07-21 22:06:30 -0700

    meals updates

--- src/components/show/BoxShow.js
+++ src/components/show/BoxShow.js
@@ -17,26 +17,32 @@ class BoxShow extends React.Component {
   }
 
   render() {
-    return (
+    const itemsArray = this.state.items;
+    let boxes = itemsArray.filter(
+      (item) => item.product_type === "market box "
+    );
+    return boxes.map((box) => (
       <Card border="secondary" style={{ width: "18rem" }}>
         <Card.Img
           variant="top"
-          src="https://techcrunch.com/wp-content/uploads/2017/04/plated_panko_crusted_salmon_recipe.jpg?w=430&h=230&crop=1"
+          src="https://images.squarespace-cdn.com/content/v1/5e7496643b423e725626f39f/1584744583760-ZSKS6MLBBB4HEG8S5H23/ke17ZwdGBToddI8pDm48kPTrHXgsMrSIMwe6YW3w1AZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0k5fwC0WRNFJBIXiBeNI5fKTrY37saURwPBw8fO2esROAxn-RKSrlQamlL27g22X2A/f2a82b_b1f5b5ab7a9a44bf94cf6db70e669d93_mv2_d_4608_2592_s_4_2.jpg?format=2500w"
         />
         <Card.Body>
-          <Card.Title>Market Box Offerings This Week </Card.Title>
+          <Card.Title>{box.name} </Card.Title>
           <Card.Text>
-            <div>about this box</div>
-            <div>Delivery Date: </div>
-            <div>Price</div>
+            <div>
+              What's included in this week's box:
+              <div>{box.description}</div>
+            </div>
+            <div>Delivery Date: {box.delivery_date} </div>
+            <div>Price: ${box.price}.00</div>
           </Card.Text>
         </Card.Body>
         <Button className="button" href="/meal">
           Add to Cart
         </Button>
       </Card>
-    );
-    // }
+    ));
   }
 }
 
--- src/components/show/MealShow.js
+++ src/components/show/MealShow.js
@@ -18,8 +18,17 @@ class MealShow extends React.Component {
   }
 
   render() {
+    const mealsArray = this.state.items;
+    let meals = mealsArray.filter(
+      (meal) => meal.product_type === "drop off meal "
+    );
+
+    let mains = meals.filter((meal) => meal.category === "Main ");
+    let sides = meals.filter((meal) => meal.category === "Sides");
+    let desserts = meals.filter((meal) => meal.category === "Dessert ");
+
     return (
-      <Card border="secondary" style={{ width: "18rem" }}>
+      <Card border="secondary" style={{ width: "25rem" }}>
         <Card.Img
           variant="top"
           src="https://techcrunch.com/wp-content/uploads/2017/04/plated_panko_crusted_salmon_recipe.jpg?w=430&h=230&crop=1"
@@ -27,9 +36,24 @@ class MealShow extends React.Component {
         <Card.Body>
           <Card.Title>This Week's Meal</Card.Title>
           <Card.Text>
-            <div>Main</div>
-            <div>Sides</div>
-            <div>Dessert</div>
+            <div>
+              <h5>Main:</h5>
+              {mains.map((main) => (
+                <div>{main.name}</div>
+              ))}
+            </div>
+            <div>
+              <h5>Sides:</h5>
+              {sides.map((side) => (
+                <div>{side.name}</div>
+              ))}
+            </div>
+            <div>
+              <h5>Dessert:</h5>
+              {desserts.map((dessert) => (
+                <div>{dessert.name}</div>
+              ))}
+            </div>
             <div>Delivery Date: </div>
             <div>Price</div>
           </Card.Text>
